b397d52f0f885a42e0103694562dfc9581ef2ec3
DooHyeon Lee <0216top@uos.ac.kr>
2026-08-11 19:09:16 +0900

    vite.config.ts 수정

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/travel-log/',
+    base: '/travel_log/',
 
     plugins: [react(), tailwindcss()],
     resolve: {
